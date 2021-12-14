01b4cf09cf9e5e1536b3ffe7371b192847847216
Erika Heidi <erika@erikaheidi.com>
2021-12-14 17:06:58 +0100

    Updated PHP CS Fixer

---
+++ .php-cs-fixer.php
@@ -0,0 +1,13 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in([__DIR__ . '/src', __DIR__ . '/tests'])
+    ->name('*.php')
+;
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+    '@PSR12' => true,
+    'strict_param' => true,
+    'array_syntax' => ['syntax' => 'short'],
+])->setFinder($finder);
